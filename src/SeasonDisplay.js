383c0b8da03768b525ca39de95070229c0bb0595
Marty Bonacci <martybonacci@gmail.com>
2019-07-13 16:41:41 -0600

    refactored logic into config values and added some styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import './SeasonDisplay.css'
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        seasonIcon: "sun"
+    },
+    winter: {
+        text: "Burr it's chilly",
+        seasonIcon: "snowflake"
+    }
+}
 
 const getSeason = (lat, month) => {
     return lat > 0 ? month > 2 && month < 9 ? "summer" : "winter" : month > 2 && month < 0 ? "winter" : "summer"
 }
 
 const SeasonDisplay = ({lat}) => {
-    const season = getSeason(lat, new Date().getMonth());
-    return <div>{season}</div>
+
+const season = getSeason(lat, new Date().getMonth());
+const {text, seasonIcon} = seasonConfig[season];
+
+    return (
+        <>
+    <i className={`${seasonIcon} icon massive icon-left`}></i>
+    <div>{text}</div>
+    <i className={`${seasonIcon} icon massive icon-right`}></i>
+    </>
+    )
 };
 
 export default SeasonDisplay;
