84d4f96445a9687bf05ed8b3e811ca320499d671
Marty Bonacci <martybonacci@gmail.com>
2019-07-13 15:41:14 -0600

    refactor constuctor and added ternary for loading and error message

--- src/index.js
+++ src/index.js
@@ -4,29 +4,21 @@ import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
-    constructor (props) {
-        super(props);
 
-        this.state = {lat: null};
+    state = {lat: null, errMessage: ''};
 
+    componentDidMount () {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({lat: position.coords.latitude});
-            },
-            (err) => console.log(err.code, err.message)
+            (position) => this.setState({lat: position.coords.latitude}),
+            (err) => this.setState({errMessage: err.message})
         );
-        
     }
 
 
-
     render() {
-
-
-    
         return (
         <div>
-            Latitude: {this.state.lat}
+            {this.state.errMessage ? `Error: ${this.state.errMessage}` : this.state.lat ? `Latitude: ${this.state.lat}` : <div>Loading...</div>}
          <SeasonDisplay/>   
         </div>
         )
